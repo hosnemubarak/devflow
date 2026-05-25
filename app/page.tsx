a75e86f2d39469ebdc44d675394a3338491275aa
Hosne Mubarak <h.m.tasmir.cse@gmail.com>
2026-05-25 14:36:44 +0600

    Asset and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <h1 className="text-3xl text-violet-700 font-bold ">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-violet-700 ">Hello world!</h1>
+      <h1 className="text-3xl font-bold text-violet-700 space-grotesk">
+        Hello again!
+      </h1>
+    </>
+  );
 }
